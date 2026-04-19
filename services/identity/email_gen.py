dc66e24e426b0cd31a43238d69ceac94ab186ff4
shafe123 <ethan.h.shafer@gmail.com>
2026-04-19 15:12:59 -0400

    fixes ruff formatting

--- services/identity/email_gen.py
+++ services/identity/email_gen.py
@@ -1,7 +1,7 @@
-'''
+"""
 This script decompresses the authors file, adds and email field to each author record based
-on their name, and then recompresses the file. 
-'''
+on their name, and then recompresses the file.
+"""
 
 import orjson
 import gzip
@@ -13,31 +13,44 @@ output_file = "dtic_authors_001_with_emails.jsonl.gz"
 
 
 # Email variations for mocking up email addresses based on author names and common domains.
-domains = ["dtic.mil", "navy.mil", "army.mil", "af.mil", "usmc.mil", "university.edu", "us.gov"]
-email_punc = ['.', '-', '_']
+domains = [
+    "dtic.mil",
+    "navy.mil",
+    "army.mil",
+    "af.mil",
+    "usmc.mil",
+    "university.edu",
+    "us.gov",
+]
+email_punc = [".", "-", "_"]
+
 
 def generate_email(name):
     # Generate a mock email address based on the author's name and random punctuation.
-    return "".join(
-        name.replace('.', '').lower().replace(' ', random.choice(email_punc))
-    ) + f"@{random.choice(domains)}"
+    return (
+        "".join(name.replace(".", "").lower().replace(" ", random.choice(email_punc)))
+        + f"@{random.choice(domains)}"
+    )
+
 
 try:
     # Check if the input file exists before attempting to process it.
     if not os.path.isfile(input_file):
-        raise FileNotFoundError(f"The file {input_file} does not exist. Please check the path and try again.")
+        raise FileNotFoundError(
+            f"The file {input_file} does not exist. Please check the path and try again."
+        )
     with gzip.open(input_file, "rb") as f:
         with gzip.open(output_file, "wb", compresslevel=5) as out_f:
             # Processes data line by line to avoid loading the entire file into memory
             for line in f:
                 data = orjson.loads(line)
-                email = generate_email(data['name'])
-                data['email'] = email
+                email = generate_email(data["name"])
+                data["email"] = email
 
                 # Write the modified data back to the output file in JSON Lines format.
-                out_f.write(orjson.dumps(data) + b'\n')
+                out_f.write(orjson.dumps(data) + b"\n")
     print(f"Processing complete. Output written to {output_file}")
-    
+
     # Swap the original file with the new file if successful.
     os.replace(output_file, input_file)
 
